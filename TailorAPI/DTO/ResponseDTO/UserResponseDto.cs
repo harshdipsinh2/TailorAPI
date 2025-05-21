a3b4544e986f05a835db0dda1f4c02c7769f6bf6
harshdipsinh2 <113231737+harshdipsinh2@users.noreply.github.com>
2025-05-21 11:28:48 +0530

    order changes

--- TailorAPI/DTO/ResponseDTO/UserResponseDto.cs
+++ TailorAPI/DTO/ResponseDTO/UserResponseDto.cs
@@ -2,6 +2,7 @@
 {
     public class UserResponseDto
     {
+        public int UserID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string MobileNo { get; set; }
